4dfb447e50986360068a01ddd59b5b8840acf0aa
Burb <352217555@qq.com>
2020-11-24 22:40:49 +0800

    Add support for controlling RS485 end elfin robot IO
    Use command: roslaunch elfin_robot_bringup elfin_ros_control_v3.launch to control the RS485 end robot IO.

---
+++ elfin_ethercat_driver_v3/include/elfin_ethercat_driver_v3/elfin_ethercat_io_client_v3.h
@@ -0,0 +1,107 @@
+/*
+Created on Tus Nov 17 15:36 2020
+
+@author: Burb
+
+ Software License Agreement (BSD License)
+
+ Copyright (c) 2020, Han's Robot Co., Ltd.
+ All rights reserved.
+
+ Redistribution and use in source and binary forms, with or without
+ modification, are permitted provided that the following conditions
+ are met:
+
+  * Redistributions of source code must retain the above copyright
+    notice, this list of conditions and the following disclaimer.
+  * Redistributions in binary form must reproduce the above
+    copyright notice, this list of conditions and the following
+    disclaimer in the documentation and/or other materials provided
+    with the distribution.
+  * Neither the name of the copyright holders nor the names of its
+    contributors may be used to endorse or promote products derived
+    from this software without specific prior written permission.
+
+ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ POSSIBILITY OF SUCH DAMAGE.
+*/
+// author: Burb
+
+#ifndef ELFIN_ETHERCAT_IO_CLIENT_V3_H
+#define ELFIN_ETHERCAT_IO_CLIENT_V3_H
+
+#include <ros/ros.h>
+#include <vector>
+#include <elfin_ethercat_driver_v3/elfin_ethercat_manager_v3.h>
+#include <elfin_robot_msgs/ElfinIODRead.h>
+#include <elfin_robot_msgs/ElfinIODWrite.h>
+#include <std_srvs/SetBool.h>
+
+#include <pthread.h>
+#include <time.h>
+
+#include <boost/shared_ptr.hpp>
+#include <boost/thread.hpp>
+
+namespace elfin_io_txpdo {
+
+const int DIGITAL_INPUT=0;
+const int ANALOG_INPUT_CHANNEL1=1;
+const int ANALOG_INPUT_CHANNEL2=2;
+const int SMART_CAMERA_X=3;
+const int SMART_CAMERA_Y=4;
+
+}
+
+namespace elfin_io_rxpdo {
+
+const int DIGITAL_OUTPUT=0;
+
+}
+
+namespace  elfin_ethercat_driver_v3 {
+
+class ElfinEtherCATIOClient{
+
+private:
+    EtherCatManager* manager_;
+    ros::NodeHandle n_;
+    ros::NodeHandle io_nh_;
+    std::vector<ElfinPDOunit> pdo_input; // txpdo
+    std::vector<ElfinPDOunit> pdo_output; //rxpdo
+    int slave_no_;
+
+    ros::ServiceServer read_sdo_; //20201116
+    ros::ServiceServer write_sdo_; //20201117
+    ros::ServiceServer get_txsdo_server_;//20201120
+    ros::ServiceServer get_rxsdo_server_;//20201120
+
+public:
+    ElfinEtherCATIOClient(EtherCatManager* manager, int slave_no, const ros::NodeHandle& nh, std::string io_port_name);
+    ~ElfinEtherCATIOClient();
+    int32_t readSDO_unit(int n); // 20201117
+    void writeOutput_unit(int n, int32_t val);
+    int32_t writeSDO_unit(int n); // 20201117
+
+    std::string getTxSDO();
+    std::string getRxSDO();
+
+    bool readSDO_cb(elfin_robot_msgs::ElfinIODRead::Request &req, elfin_robot_msgs::ElfinIODRead::Response &resp); // 20201117
+    bool writeSDO_cb(elfin_robot_msgs::ElfinIODWrite::Request &req, elfin_robot_msgs::ElfinIODWrite::Response &resp); // 20201117
+    bool getRxSDO_cb(std_srvs::SetBool::Request &req, std_srvs::SetBool::Response &resp);
+    bool getTxSDO_cb(std_srvs::SetBool::Request &req, std_srvs::SetBool::Response &resp);
+};
+
+}
+
+#endif
