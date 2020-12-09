782ad6484a5adb2166e42b62a397c10c9eec2c32
Burb <352217555@qq.com>
2020-12-09 20:34:38 +0800

    Add support for reading or writing 485 End DO,DI,LED,button.

--- elfin_ethercat_driver_v3/src/elfin_ethercat_io_client_v3.cpp
+++ elfin_ethercat_driver_v3/src/elfin_ethercat_io_client_v3.cpp
@@ -70,6 +70,7 @@ ElfinEtherCATIOClient::ElfinEtherCATIOClient(EtherCatManager *manager, int slave
 
     // Initialize services
     read_sdo_=io_nh_.advertiseService("read_di", &ElfinEtherCATIOClient::readSDO_cb, this); // 20201117: support for 485 end
+    read_do_=io_nh_.advertiseService("read_do", &ElfinEtherCATIOClient::readDO_cb, this); // 20201130: support for read DO
     write_sdo_=io_nh_.advertiseService("write_do",&ElfinEtherCATIOClient::writeSDO_cb, this); // 20201117: support for 485 end
     get_txsdo_server_=io_nh_.advertiseService("get_txpdo", &ElfinEtherCATIOClient::getTxSDO_cb, this); // 20201120: support for 485 end
     get_rxsdo_server_=io_nh_.advertiseService("get_rxpdo", &ElfinEtherCATIOClient::getRxSDO_cb, this); // 20201120: support for 485 end
@@ -96,6 +97,26 @@ int32_t ElfinEtherCATIOClient::readSDO_unit(int n)
     // 0x2126, L_4 is end DI, H_4 is button DI.
     int32_t map;
     map = (manager_->readSDO<int32_t>(3, 0x2126, 0x0)) << 16; // read the end DI
+    manager_->writeSDO<int>(3,0x3100,0x0,0); // Modbus DO command 0
+    usleep(50000);
+    return map;
+}
+
+// 20201130: read the end DO
+int32_t ElfinEtherCATIOClient::readDO_unit(int n)
+{
+    if(n<0 || n>=pdo_input.size())
+        return 0x0000;
+    // 20201130: build the connection.
+    manager_->writeSDO<int>(3,0x3100,0x0,1); // Modbus DO command
+    usleep(50000);
+    manager_->writeSDO<int32_t>(3,0x3101,0x0,0x010040); // 64 connect to Modbus
+    usleep(50000);
+    manager_->writeSDO<int32_t>(3,0x3102,0x0,0x010001); // Modbus Addr & count
+    usleep(50000);
+    // 0x310C, DO.
+    int32_t map;
+    map = (manager_->readSDO<int32_t>(3, 0x310C, 0x0)) << 12; // read the end DO
     manager_->writeSDO<int>(3,0x3100,0x0,0); // Modbus DO command 0
     usleep(50000);
     return map;
@@ -163,6 +184,13 @@ std::string ElfinEtherCATIOClient::getRxSDO()
 bool ElfinEtherCATIOClient::readSDO_cb(elfin_robot_msgs::ElfinIODRead::Request &req, elfin_robot_msgs::ElfinIODRead::Response &resp)
 {
     resp.digital_input=readSDO_unit(elfin_io_txpdo::DIGITAL_INPUT);
+    return true;
+}
+
+// 20201130: read the end DO
+bool ElfinEtherCATIOClient::readDO_cb(elfin_robot_msgs::ElfinIODRead::Request &req, elfin_robot_msgs::ElfinIODRead::Response &resp)
+{
+    resp.digital_input=readDO_unit(elfin_io_txpdo::DIGITAL_INPUT); // 20201130: digital_input for convenience
     return true;
 }
 
